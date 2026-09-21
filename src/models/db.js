aea9fcf3af053dabbf50e04f193c4c678d69a17b
Maccharmy <musanga@byupathway.edu>
2026-09-21 05:58:36 +0100

    debug

--- src/models/db.js
+++ src/models/db.js
@@ -1,5 +1,8 @@
 import { Pool } from 'pg';
 
+// Debug: print out the DB_URL being used
+console.log("DB_URL:", process.env.DB_URL);
+
 const pool = new Pool({
     connectionString: process.env.DB_URL,
     ssl: { rejectUnauthorized: false }
