0b76b0e7f31befc6fac6dd30d9214559268d7320
Maccharmy <musanga@byupathway.edu>
2026-09-23 09:00:47 +0100

    Final W02 assignment setup

--- src/models/projects.js
+++ src/models/projects.js
@@ -2,14 +2,13 @@ import db from './db.js';
 
 const getAllProjects = async () => {
   const query = `
-    SELECT s.service_id, s.title, s.description, s.location, s.date, o.name AS organization_name
-    FROM services s
-    JOIN organizations o ON s.organization_id = o.organization_id
-    ORDER BY s.date;
+    SELECT p.project_id, p.title, p.description, p.location, p.date, o.name AS organization_name
+    FROM project p
+    JOIN organization o ON p.organization_id = o.organization_id
+    ORDER BY p.date;
   `;
   const result = await db.query(query);
   return result.rows;
 };
 
-// ✅ ES Module export
 export { getAllProjects };
